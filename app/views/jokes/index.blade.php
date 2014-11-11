1c6238cb487cc25e3c6dfeda2222b2a649b98c9c
Martin Černý <cerny.mrtn@gmail.com>
2014-11-11 21:39:13 +0000

    link to unijokes

--- app/views/jokes/index.blade.php
+++ app/views/jokes/index.blade.php
@@ -3,8 +3,10 @@
 @section('about-us')
     <h1>The Best {{{ $categoryTitle or '' }}} jokes</h1>
     <p>
-        Welcome to the collected jokes by Martin Černý.
-        I have selected only the best jokes. On this website you will not find any stupid one. 
+        <?php if(!isset($categoryTitle) && isset($page) && $page <= 1) { ?>
+            Welcome to the collected jokes by Martin Černý.
+            I have selected only the best jokes. <br>On this website you will not find any stupid one. 
+        <?php } ?>
     </p>
 @stop
 
@@ -15,13 +17,18 @@
 @stop
 
 @section('pagination')
+    <?php if($jokes->getTotal() > 10) { ?>
     <section>
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-center">
                     <?php echo (str_replace("?page=", "/", str_replace("$page?page=", '', $jokes->links()))) ?>
                 </div>
+                <div class="col-md-12 text-center bottom-buffer">
+                    <a href="http://www.unijokes.com" class="talmer" style="text-decoration: none">Try more Jokes from Talmer</a>
+                </div>
             </div>
         </div>
     </section>
+<?php } ?>
 @stop
