94e8b82a908630a3597c4971e62f6dbed245cf32
Martin Černý <cerny.mrtn@gmail.com>
2014-11-19 21:29:46 +0000

    fixup! queries moved from controller to model

--- app/controllers/JokeController.php
+++ app/controllers/JokeController.php
@@ -2,148 +2,165 @@
 
 class JokeController extends BaseController {
 
+    /**
+     * 
+     * @param type $page
+     * @return type
+     */
     public function getIndex($page = 0) {
-        $categories = $this->getCategories();
+        $categories = Category::getAll();
         
-        $data = $this->getByPage($page);
+        $data = Joke::getByPage($page);
         Paginator::setCurrentPage($page);
         $jokes = Paginator::make($data->items, $data->totalItems, 10);
-
-        return View::make('jokes.index', array('jokes' => $jokes, 'page' => $page, 'categories' => $categories));
-    }
-
-    public function getCategory($category, $page = 0) {
-        $categoryTitle = str_replace('-', ' ', str_replace('-jokes', '', $category));
-        $categories = $this->getCategories();
-        
-        $data = $this->getByPage($page, $categoryTitle);
-        Paginator::setCurrentPage($page);
-        $jokes = Paginator::make($data->items, $data->totalItems, 10);
-        
-        return View::make('jokes.index', array('jokes' => $jokes, 'page' => $page, 'categories' => $categories, 'categoryTitle' => $categoryTitle));
-    }
-    
-    public function getJoke($id) {
-        $joke = DB::select("SELECT joke.*, GROUP_CONCAT(category.name) as categories FROM joke JOIN jokecategory ON joke.id = jokecategory.joke_id JOIN category ON jokecategory.category_id = category.id WHERE joke.id = '$id'");
-        $categories = $this->getCategories();
-        return View::make('joke.index', array('joke' => $joke[0], 'categories' => $categories));
+        return View::make('jokes.index', array('jokes' => $jokes, 'page' => $page, 'categories' => $categories, 'totalItems' => $data->totalItems));
     }
 
     /**
      * 
-     * @return array
+     * @param type $category
+     * @param type $page
+     * @return type
      */
-    private function getCategories() {
-        return DB::select("SELECT name, id, main FROM category ORDER BY name");
+    public function getCategory($category, $page = 0) {
+        $categoryTitle = str_replace('-', ' ', str_replace('-jokes', '', $category));
+        $categories = Category::getAll();
+        
+        $data = Joke::getByPage($page, $categoryTitle);
+        Paginator::setCurrentPage($page);
+        $jokes = Paginator::make($data->items, $data->totalItems, 10);
+        
+        return View::make('jokes.index', array('jokes' => $jokes, 'page' => $page, 'categories' => $categories, 'categoryTitle' => $categoryTitle, 'totalItems' => $data->totalItems));
+    }
+    
+    /**
+     * Get specific joke
+     * @param type $id
+     * @return type
+     */
+    public function getJoke($id) {
+        $joke = Joke::getById($id);
+        $categories = Category::getAll();
+        return View::make('joke.index', array('joke' => $joke[0], 'categories' => $categories, 'type' => 'joke'));
     }
 
     /**
-     * Get results by page
-     *
-     * @param int $page
-     * @param String $category
-     * @return StdClass
+     * Get all categories
+     * @return array
      */
-    private function getByPage($page = 0, $category = 0) {
-        if($page != 0) {
-            $start = ($page-1) * 10;
-        } else {
-            $start = 0;
-        }
-        $results = new stdClass();
-        $results->page = $page;
-        $results->limit = 10;
-        $results->totalItems = 0;
-        $results->items = array();
-        $totalItems = 0;
-
-        if ($category != '') {
-            $results->items = DB::select("SELECT joke.*, GROUP_CONCAT(category.name) as categories FROM joke JOIN jokecategory ON joke.id = jokecategory.joke_id JOIN category ON jokecategory.category_id = category.id WHERE category.name = '$category' GROUP BY joke.id ORDER BY plus_votes - minus_votes DESC LIMIT 10 OFFSET $start");
-            $totalItems = DB::select("SELECT COUNT(*) as count FROM joke JOIN jokecategory ON joke.id = jokecategory.joke_id JOIN category ON jokecategory.category_id = category.id WHERE category.name = '$category'");
-        } else {
-            $results->items = DB::select("SELECT joke.*, GROUP_CONCAT(category.name) as categories FROM joke JOIN jokecategory ON joke.id = jokecategory.joke_id JOIN category ON jokecategory.category_id = category.id GROUP BY joke.id ORDER BY plus_votes - minus_votes DESC LIMIT 10 OFFSET $start");
-            $totalItems = DB::select("SELECT COUNT(*) as count FROM joke");
-        }
-        $results->totalItems = $totalItems[0]->count;
-
-        return $results;
+    private function getCategories() {
+        return Category::getAll();
     }
     
+    /**
+     * 
+     * @return type
+     */
     public function addJoke() {
         return View::make('joke.add');
     }
     
+    
+    /**
+     * 
+     * @param type $id
+     * @return type
+     */
     public function editJoke($id) {
-        $joke = DB::select("SELECT joke.*, category.name as categories FROM joke JOIN jokecategory ON joke.id = jokecategory.joke_id JOIN category ON jokecategory.category_id = category.id WHERE joke.id = '$id'");        
-        $joke = Joke::find(1);
-        return View::make('joke.add', array('joke' => $joke));
+        $joke = Joke::with('categories')->findOrFail($id);
+        $joke['categories'] = array_pluck($joke['categories'], 'id');
+        $previous = Input::get('previous');
+        return View::make('joke.add', array('joke' => $joke, 'previous' => $previous));
     }
     
+    public function updateJoke($id) {
+        $inputs = Input::get();
+        $joke = Joke::findOrFail($id);
+        DB::beginTransaction();
+        try {
+            $joke->update($inputs);
+            $joke->categories()->detach();    
+            $categoriesSelected = Input::get('categories');
+            $joke->categories()->attach($categoriesSelected); 
+            
+            DB::commit();
+            return Redirect::to(Input::get('previous'));
+        } catch (\Exception $e) {
+            DB::rollback();
+            return Redirect::back()->with('fail', 'An error occurred saving the joke. Please contact administrator')->withInput();   
+        }  
+    }
+    
+    /**
+     * 
+     * @return type
+     */
     public function insertJoke() {
-        $title = Input::get('title');
-        $text = Input::get('text');
+        $newJoke = new Joke();
+        $newJoke->title = Input::get('title');
+        $newJoke->text = Input::get('text');
         $categoriesSelected = Input::get('categories');
         
-        if (empty($categoriesSelected) || empty($text) || empty($title)) {
-            return View::make('joke.add', array('message' => "You should fill all inputs.", 'type' => "danger", "title" => $title, "text" => $text));
+        if (empty($categoriesSelected) || empty($newJoke->text) || empty($newJoke->title)) {
+            return View::make('joke.add', array('message' => "You should fill all inputs.", 'type' => "danger", "joke" => $newJoke));
         }
         
         $jokes = Joke::all();
         foreach ($jokes as $joke) { 
-            similar_text(strtoupper($text), strtoupper($joke->text), $similarity_text);
-            similar_text(strtoupper($title), strtoupper($joke->title), $similarity_title);
+            similar_text(strtoupper($newJoke->text), strtoupper($joke->text), $similarity_text);
+            similar_text(strtoupper($newJoke->title), strtoupper($joke->title), $similarity_title);
             if (number_format($similarity_text, 0) > 70){ 
                 $message = "<strong>The text you entered is too similar to joke:</strong> $joke->text"; 
             } else if (number_format($similarity_title, 0) > 90) {
                 $message = "<strong>The title you entered is too similar to title:</strong> $joke->title"; 
             }
             if(isset($message)) {
-                return View::make('joke.add', array('message' => $message, 'type' => "danger", "title" => $title, "text" => $text));
+                return View::make('joke.add', array('message' => $message, 'type' => "danger", "joke" => $newJoke));
             }
         }
- 
-        $joke = new Joke();
-        $joke->title = $title;
-        $joke->text = $text;
-        $joke->save();
+        $newJoke->save();
               
-        $joke->categories()->attach($categoriesSelected);
+        $newJoke->categories()->attach($categoriesSelected);
 
         return View::make('joke.add', array('message' => "successfull",  'type' => "success"));
     }
     
-    private function isTitleDuplicit($title) {
-        $jokes = Joke::all();
-        foreach ($jokes as $joke) { 
-            similar_text(strtoupper($title), strtoupper($joke->title), $similarity_title);
-            if (number_format($similarity_title, 0) > 90) {
-                return true;
-            }
-        }
-        return false;    
+    /**
+     * 
+     * @param type $id
+     * @return type
+     */
+    public function deleteJoke($id) {
+        $joke = Joke::find($id);
+        $joke->delete();
+        return Redirect::to(Input::get('previous'));
     }
     
-    private function isTextDuplicit($text) {
-        $jokes = Joke::all();
-        foreach ($jokes as $joke) { 
-            similar_text(strtoupper($text), strtoupper($joke->text), $similarity_text);
-            if (number_format($similarity_text, 0) > 70){ 
-                return true;
-            }
-        }
-        return false;
-    }
     
+    /**
+     * 
+     * @return type
+     */
     public function checkTitleDuplicity() {
         $title = Input::get('title');
-        return json_encode($this->isTitleDuplicit($title));
+        return json_encode(Joke::isTitleDuplicit($title));
     }
-        
+     
+    
+    /**
+     * 
+     * @return type
+     */
     public function checkTextDuplicity() {
         $text = Input::get('text');
-        return json_encode($this->isTextDuplicit($text));
+        return json_encode(Joke::isTextDuplicit($text));
     }
 
+    
+    /**
+     * 
+     * @return type
+     */
     public function addVote() {
         $return = array();
         $return['type'] = Input::get('type');
